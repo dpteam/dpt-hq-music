9a4bfa2f8d68577f173c053b572416de1c226de4
DartPower <SIMPSON-NEKEK@mail.ru>
2020-09-01 20:46:39 +0300

    Микрофикс батника

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 @echo off
 SetLocal EnableExtensions EnableDelayedExpansion
-color 09
-title Compressing. Please wait... [Approximately 1-2 mins]
+color 0B
+title Compressing, please wait... [Approximately 1-2 mins]
+echo Building DPT.HQ Music Addon component...
 cd pk3_data
 ..\7z a -t7z -m0=lzma2 -mx=9 -aoa "..\DPT-HQ-MusicAddon-%date:~6,4%%date:~3,2%%date:~0,2%-%time:~0,2%%time:~3,2%.pk7" *
 cls
